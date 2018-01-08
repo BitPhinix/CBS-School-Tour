d87d3a922c2b88832cd54edc81df49c61a7b5d46
BitPhinix <meier.eric2@t-online.de>
2018-01-08 23:26:48 +0100

    A tone is working, a tone is not ^^

--- src/vtour/uiElements/map2d.ts
+++ src/vtour/uiElements/map2d.ts
@@ -2,12 +2,14 @@
 import SvgJs = require("svg.js");
 import {RoomLocation} from "../Components/autoComplete";
 import {Navigator} from "../navigator";
+import PanZoom = require("svg-pan-zoom");
 
 export const Map2d  = {
     SvgContainer: $("#Map2dContainer"),
     SvgElement: undefined,
     SvgPanZoom: undefined,
     DrawContainer: undefined,
+    Ready: false,
 
     init: function() {
         this.loadFloor(1);
@@ -26,7 +28,9 @@ export const Map2d  = {
     },
 
     loadSvg: function (path: string) {
-        const svgElement = SvgJs('Map2dContainer');
+        this.SvgContainer.empty();
+
+        const svgElement = SvgJs('Map2dContainer').attr("id","Map2d");
 
         $.get(path, function(contents) {
             const $tmp = $('svg', contents);
@@ -35,17 +39,45 @@ export const Map2d  = {
 
         this.DrawContainer = svgElement.group();
         this.SvgElement = svgElement;
+        this.Ready = false;
+
+        $("#Map2d").on("click",() => this.initPanZoom());
     },
 
     navigate: function (start: RoomLocation, end: RoomLocation) {
+        this.loadFloor(start.floor);
+
         const result = Navigator.navigateGlobal(start, end);
 
         for (let i = 1; i < result.length; i++)
             this.drawLine(result[i - 1], result[i]);
     },
 
+    initPanZoom: function() {
+        if(this.Ready)
+            return;
+
+        this.SvgPanZoom = PanZoom("#Map2d", {
+            panEnabled: true,
+            controlIconsEnabled: false,
+            zoomEnabled: true,
+            dblClickZoomEnabled: true,
+            mouseWheelZoomEnabled: true,
+            preventMouseEventsDefault: true,
+            zoomScaleSensitivity: 0.2,
+            minZoom: 0.5,
+            maxZoom: 3,
+            fit: false,
+            contain: false,
+            center: false,
+            refreshRate: 'auto',
+            eventsListenerElement: null
+        });
+
+        this.Ready = true;
+    },
+
     lookTo: function(floorId: number, x: number, y: number) {
-        //Show corresponding floor
         this.SvgPanZoom.reset();
 
         let sizes = this.SvgPanZoom.getSizes();
